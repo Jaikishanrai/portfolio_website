7b73feadd93fecb4d40844e7a75dc1475cf33b2b
jai <jk007rai@gmail.com>
2023-06-07 10:22:02 +0530

    Add app description, image, and links.

--- main.py
+++ main.py
@@ -25,16 +25,22 @@ you can find some of the apps i have built in python. Feel free to contact me!
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("006 data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("006 images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("006 images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
